fcb9dca94062c7854be39a1f02ec8923c9835349
Danilo B <buncho.gamingnetwork@gmail.com>
2017-11-26 02:14:27 +0100

    Added inventory , item scripts, added working inventory system

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private Dictionary<ItemType, List<Item>> inventory = new Dictionary<ItemType, List<Item>>();
+    /*
+     Output: "ItemType" , <items>
+     Example: "Food", <hambruger, frietjes, honing>
+     */
+
+    private void Start()
+    {
+        Item it = new Item();
+        for (int i = 0; i < 5; i++)
+        {
+            Item apple = new Item(0, ItemType.food, "Apple" + i);
+            it = apple;
+            addItem(apple);
+        }
+
+        Debug.Log(inventory[ItemType.food].Count);
+        deleteItem(it);
+        Debug.Log(inventory[ItemType.food].Count);
+
+    }
+
+    private void addItem(Item item)
+    {
+        // if the item type doens't exist in the inventory dictionary, we add it.
+        if (!containsItemType(item.Type))
+        {
+            // adding the category.
+            inventory.Add(item.Type, new List<Item>());
+            // adding the item.
+            inventory[item.Type].Add(item);
+            return;
+        }
+
+        inventory[item.Type].Add(item);
+
+    }
+
+    private void deleteItem(Item item)
+    {
+        // check if the item type exist in the inventory, if not stop the function.
+        if (!containsItemType(item.Type))
+            return;
+
+        // loop through the list.
+        for (int i = 0; i < inventory[item.Type].Count; i++)
+        {
+            Item currentItem = inventory[item.Type][i];
+
+            // check if the item ID and NAME are the same, if not stop.
+            if (currentItem.ID != item.ID && currentItem.Name != item.Name)
+                return;
+
+            inventory[item.Type].Remove(item);
+            Debug.Log("Deleted: " + "ID: " + item.ID + "Name: " + item.Name);
+        }
+    }
+
+    private bool containsItemType(ItemType type)
+    {
+        return inventory.ContainsKey(type);
+    }
+
+
+}
---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ItemType
+{
+    ZERO,
+    weapons,
+    food
+}
+
+public class Item {
+
+    public Item(int itemId, ItemType itemType, string itemName)
+    {
+        this.id = itemId;
+        this.type = itemType;
+        this.name = itemName;
+    }
+
+    public Item()
+    {
+        this.id = -1;
+        this.type = ItemType.ZERO;
+        this.name = "Null_Item";
+    }
+
+    private int id;
+    public int ID
+    {
+        get { return id; }
+    }
+
+    private ItemType type;
+    public ItemType Type
+    {
+        get { return type; }
+    }
+
+    private string name;
+    public string Name
+    {
+        get { return name; }
+    }
+}
